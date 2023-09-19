d121aa3d0fc66df4555cf67a03b2594d40ffc296
Gaurav Pratap Singh Rathor <135633616+gaurav-pratap-singh-rathor@users.noreply.github.com>
2023-09-19 21:03:22 +0530

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'Gaurav', 'Testing TODO', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'Pratap', 'First Todo', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'Singh', 'Singh Todo', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'Rathore', 'Todo by Rathore', CURRENT_DATE(), false);
